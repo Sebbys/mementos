dec7a5cfb7d042d84f640fd40e0d086701e3cea7
Sebbys <raynosebastian19@gmail.com>
2023-11-14 00:40:56 +0700

    route auth

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,7 +1,16 @@
 import Link from 'next/link';
 import { buttonVariants } from '@/components/ui/button';
+import { type CurrentUser } from '@/app/types';
+import { redirect } from 'next/navigation';
+import { getUser } from '../server/user';
 
 export default async function Courses() {
+  const currentUser = (await getUser()) as CurrentUser;
+
+  if (currentUser?.roles == 'PUBLIC' || !currentUser) {
+    redirect('/cursed');
+  }
+
   return (
     <div className='flex flex-row gap-x-2 mt-4 justify-center prose prose-xl'>
       <Link className={buttonVariants({ variant: 'secondary' })} href={'/courses/tags/analytics'}>
